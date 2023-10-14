82bc169bedaa200aa00907339315120ac2afdb6e
Vishwa Vengadesh <110449434+VishwaDax@users.noreply.github.com>
2023-10-14 15:17:44 +0530

    Update main.m

--- Calculator/Calculator/main.m
+++ Calculator/Calculator/main.m
@@ -1,10 +1,4 @@
-//
-//  main.m
-//  Calculator
-//
-//  Created by Aaron Caines on 28/08/2018.
-//  Copyright © 2018 Aaron Caines. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
