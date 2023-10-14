73f756995779690f13fa2f5051450f05e5c81f22
Vishwa Vengadesh <110449434+VishwaDax@users.noreply.github.com>
2023-10-14 15:16:25 +0530

    Update CalculatorUITests.m

--- Calculator/CalculatorUITests/CalculatorUITests.m
+++ Calculator/CalculatorUITests/CalculatorUITests.m
@@ -1,10 +1,4 @@
-//
-//  CalculatorUITests.m
-//  CalculatorUITests
-//
-//  Created by Aaron Caines on 28/08/2018.
-//  Copyright © 2018 Aaron Caines. All rights reserved.
-//
+
 
 #import <XCTest/XCTest.h>
 
