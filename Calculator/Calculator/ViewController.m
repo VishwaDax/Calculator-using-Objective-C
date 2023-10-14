f0b9aed32f54dbc13f1478c1602faf4493a96b29
Vishwa Vengadesh <110449434+VishwaDax@users.noreply.github.com>
2023-10-14 15:14:25 +0530

    Update ViewController.m

--- Calculator/Calculator/ViewController.m
+++ Calculator/Calculator/ViewController.m
@@ -1,10 +1,4 @@
-//
-//  ViewController.m
-//  Calculator
-//
-//  Created by Aaron Caines on 28/08/2018.
-//  Copyright © 2018 Aaron Caines. All rights reserved.
-//
+
 
 #import "ViewController.h"
 
